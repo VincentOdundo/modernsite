5cb46676cb499195053674c8f79bdb7d4e848a61
VincentOdundo <64671774+VincentOdundo@users.noreply.github.com>
2023-05-10 16:09:31 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { Footer, Blog, Features, Possibility, Header, whatGPT } from './containers'
+import {CTA, Brand, Navbar } from './components'
+
+const App = () => {
+  return (
+    <div className='App'>
+        <div className='gradient__bg'>
+          <Navbar />
+          <Header/>
+        </div>
+        <Brand />
+        <whatGPT />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/containers/whatGPT/whatGpt.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './whatgpt.css'
+
+const whatGPT = () => {
+  return (
+    <div>whatGPT</div>
+  )
+}
+
+export default whatGPT
