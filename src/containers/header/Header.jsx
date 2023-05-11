0a8259734d062d243aaf311d4f0bc2d126bb1f65
VincentOdundo <64671774+VincentOdundo@users.noreply.github.com>
2023-05-11 13:21:10 +0300

    modified:   src/components/navbar/Navbar.jsx 	modified:   src/containers/header/Header.jsx 	modified:   src/containers/header/header.css

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="gpt__header section__padding" id="home">
+      <div className="gpt__header-content">
+        <h1 className="gradient__text">
+          The premier African news aggregator site powered by AI
+        </h1>
+        <p>
+          Explore the vibrant and diverse world of African breaking news,
+          politics, business, technology, sports and entertainment. NairoBell
+          readers are informed and up-to-date on all the latest happenings in
+          Africa.
+        </p>
+        <div className="gpt__header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt__header-content__people">
+          <img src={people} alt="people" />
+          <p>1600 people requested access in the last 24 Hours</p>
+        </div>
+      </div>
+      <div className="gpt__header-image">
+        <img src={ai} alt="ai" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
